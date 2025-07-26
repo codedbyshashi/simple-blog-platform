41ead6f6518a1ae2c525c4ab031fe8e526fe08c1
shashidharreddy <kotashashidharreddy1@gmail.com>
2025-07-26 14:29:59 +0530

    spring initializr setup

---
+++ src/main/java/com/blogplatform/simpleblogplatform/SimpleBlogPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.blogplatform.simpleblogplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBlogPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBlogPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blogplatform/simpleblogplatform/SimpleBlogPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogplatform.simpleblogplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBlogPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
